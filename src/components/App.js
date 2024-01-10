5d0ae265a6fc3e56a01361d70f24349dfab5c06b
Nasim-ka <131031633+Nasim-ka@users.noreply.github.com>
2024-01-10 01:43:59 -0800

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Card from './Card';
+import emojipedia from '../emojipedia'
+const createCard = (emojipedia) => {
+  return (
+    <Card
+      key={emojipedia.id}
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      meaning={emojipedia.meaning}
+    />
+  );
+};
+function App() {
+  return (
+    <div>
+      <h1>
+     <span>emojipedia</span>
+     </h1>
+      {emojipedia.map(createCard)}
+    </div>
+
+  );
+}
+
+export default App;
